bd09f0bd3090a6082102b6e7f6c73c2c959d7c06
Kisskin-Mister <143715327+Kisskin-Mister@users.noreply.github.com>
2023-10-05 16:06:18 +0300

    Тестовый Калькулятор

--- src/Main.java
+++ src/Main.java
@@ -6,44 +6,45 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String line = sc.nextLine();
-        System.out.println(calc(line));
+        String input = sc.nextLine();
+        System.out.println(calc(input));
 
     }
-    public static String calc(String line){
-        line = line.replace(" ", "");
+
+    public static String calc(String input) {
+        input = input.replace(" ", "");
         char opp;
         int a;
         int b;
         int result;
-        if (line.contains("+")) {
-            String[] data = line.split("\\+");
+        if (input.contains("+")) {
+            String[] data = input.split("\\+");
             opp = '+';
-            a = valueOf(data[0]);
-            b = valueOf(data[1]);
-            if(data.length>2)
+            a = Integer.parseInt(data[0]);
+            b = Integer.parseInt(data[1]);
+            if (data.length > 2)
                 throw new IllegalArgumentException("Не может быть больше двух операндов");
 
-        } else if (line.contains("-")) {
-            String[] data = line.split("-");
+        } else if (input.contains("-")) {
+            String[] data = input.split("-");
             opp = '-';
-            a = valueOf(data[0]);
-            b = valueOf(data[1]);
-            if(data.length>2)
+            a = Integer.parseInt(data[0]);
+            b = Integer.parseInt(data[1]);
+            if (data.length > 2)
                 throw new IllegalArgumentException("Не может быть больше двух операндов");
-        } else if (line.contains("*")) {
-            String[] data = line.split("\\*");
+        } else if (input.contains("*")) {
+            String[] data = input.split("\\*");
             opp = '*';
-            a = valueOf(data[0]);
-            b = valueOf(data[1]);
-            if(data.length>2)
+            a = Integer.parseInt(data[0]);
+            b = Integer.parseInt(data[1]);
+            if (data.length > 2)
                 throw new IllegalArgumentException("Не может быть больше двух операндов");
-        } else if (line.contains("/")) {
-            String[] data = line.split("/");
+        } else if (input.contains("/")) {
+            String[] data = input.split("/");
             opp = '/';
-            a = valueOf(data[0]);
-            b = valueOf(data[1]);
-            if(data.length>2)
+            a = Integer.parseInt(data[0]);
+            b = Integer.parseInt(data[1]);
+            if (data.length > 2)
                 throw new IllegalArgumentException("Не может быть больше двух операндов");
         } else {
             throw new IllegalArgumentException("Некорректно введен оператор");
@@ -58,12 +59,11 @@ public class Main {
             result = a / b;
         }
 
-        if(!((a>10||b>10)||(a<=0||b<=0))){
-            return String.valueOf(result);}
-        else{
+        if (!((a > 10 || b > 10) || (a <= 0 || b <= 0))) {
+            return String.valueOf(result);
+        } else {
             throw new IllegalArgumentException("Введенное число больше 10 или меньше 0");
         }
-
 
 
     }
